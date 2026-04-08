14650e9f5534411e66a2c97396da9f7511af7f1c
yukihiro23243 <yukihiro23243@gmail.com>
2026-04-08 14:23:55 +0900

    Renderデプロイ用設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
 end
