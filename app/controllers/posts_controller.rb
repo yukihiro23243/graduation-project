fe30f407c37b4835b4928f136e7e9914bd5d43ed
yukihiro23243 <yukihiro23243@gmail.com>
2026-04-11 16:05:20 +0900

    記事編集機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -22,7 +24,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "投稿を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to posts_path, alert: "権限がありません" unless @post.user == current_user
+  end
 
   def post_params
     params.require(:post).permit(:title, :content, :image)
