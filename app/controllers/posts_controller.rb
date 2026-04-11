6f7cee92ccb7e5b054aaab10f1b83347c67b055f
yukihiro23243 <yukihiro23243@gmail.com>
2026-04-11 15:46:33 +0900

    記事閲覧機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def create
